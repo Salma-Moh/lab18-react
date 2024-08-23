0302b6fffc7347d2966479c2b684f1d821dda37e
Salma-Moh <salmamoh168@gmail.com>
2024-08-23 16:59:53 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Home from './components/home';
-import GuessNumber from './components/GuessNumber';
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <GuessNumber />
     </div>
   );
 }
