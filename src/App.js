d701a2156118f6b6dbc89c7279a715e5d795b6a7
Salma-Moh <salmamoh168@gmail.com>
2024-08-23 17:50:05 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './components/home';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Analytics />
     </div>
   );
 }
