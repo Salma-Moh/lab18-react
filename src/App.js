fb9cd0bc0c82c3572bf149f62b6ddd8e695ac9e9
Salma-Moh <salmamoh168@gmail.com>
2024-08-26 13:00:05 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './components/home';
+import GuessNumber from './components/GuessNumber';
 import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <GuessNumber />
       <Analytics />
     </div>
   );
